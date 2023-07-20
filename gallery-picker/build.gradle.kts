cc5681dbbba228050f40c1dc49cfa284b89c6a8f
David Trujillo <ddavid.te@gmail.com>
2023-07-20 20:43:11 +0200

    Bump lib version

--- gallery-picker/build.gradle.kts
+++ gallery-picker/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "run.nabla"
             artifactId = "gallery-picker"
-            version = "1.3.0"
+            version = "1.4.0"
             afterEvaluate {
                 from(components["release"])
             }
